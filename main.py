7c41a82bad611a1d241577a17b27575e421d2dc1
OrdiNeu <OrdinaryNeumann@gmail.com>
2017-01-20 23:38:34 -0500

    Test

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import requests
 ### CONSTANTS #################################################################
 GIT_RELOAD_EXIT_CODE = 5
 COMMAND_PREFIX = ['?', '!']
-DESCRIPTION = 'OrdiNeu\'s Discord bot for the Netrunner channel'
+DESCRIPTION = 'OrdiNeu\'s Discord bot for the Netrunner channel. Also the git reload worked'
 HELP_ATTRS = {'hidden': True}
 EXTENSIONS = [
 	'exts.Netrunner',
