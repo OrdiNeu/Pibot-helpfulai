30c95ca6d8851df50860a92df6acd9b7df6da01c
OrdiNeu <OrdinaryNeumann@gmail.com>
2017-01-22 10:08:18 -0500

    Adding some code from DannyRapptz's bot for some administrative commands

---
+++ exts/utils/checks.py
@@ -0,0 +1,11 @@
+# Various helper functions for discord.py
+# PREAMBLE ####################################################################
+import discord
+import asyncio
+
+# Checks if a message came from the administrator
+def is_admin(message):
+	return "135449740778274816" == message.author.id
+
+def is_owner():
+    return commands.check(lambda ctx: is_owner_check(ctx.message))
