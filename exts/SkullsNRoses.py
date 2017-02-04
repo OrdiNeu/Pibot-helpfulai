5d1ee6a2c714ebfcd8d71ae8d353f9444ced3197
OrdiNeu <OrdinaryNeumann@gmail.com>
2017-02-04 15:30:46 -0500

    Further work, should be a working version

--- exts/SkullsNRoses.py
+++ exts/SkullsNRoses.py
@@ -10,11 +10,17 @@ from discord.ext import commands
 
 PLAYER_HAS_NO_SUCH_ANTE = -1
 WRONG_PLAYER_RESPONDED = -2
+BET_TOO_HIGH = -3
+BET_TOO_LOW = -4
+PLAYER_ALREADY_ANTED = -5
+INIT_PHASE_OVER = -6
+WRONG_GAME_PHASE = -7
 
 PHASE_WAITING = 0
 PHASE_INITANTE = 1
 PHASE_ANTEING = 2
 PHASE_BETTING = 3
+PHASE_FLIPPING = 4
 
 class SkullsSession:
 	"""One active skulls N roses game"""
@@ -24,7 +30,10 @@ class SkullsSession:
 		self.piles = {}
 		self.addPlayer(initialplayer)
 		self.curPlayer = 0
+		self.totalTiles = 0
 		self.phase = PHASE_WAITING
+		self.lastBet = 0
+		self.lastBettingPlayer = ""
 	
 	def addPlayer(self, player):
 		"""Add a player to the game"""
@@ -38,8 +47,6 @@ class SkullsSession:
 		position = 0
 		
 		# Sanity checks
-		if self.players[self.curPlayer] != player:
-			return(WRONG_PLAYER_RESPONDED)
 		try:
 			position = int(position_str)
 		except ValueError:
@@ -47,18 +54,48 @@ class SkullsSession:
 		if len(self.decks[player.name]) >= position:
 			return(PLAYER_HAS_NO_SUCH_ANTE)
 		
-		# Push the selected tile onto the pile
-		selected = self.decks[player.name].pop(position)
-		self.piles[player.name].append(selected)
-		
-		# Swap player
-		self.curPlayer += 1
-		self.curPlayer %= len(self.players)
-		return(0)
+		if self.phase == PHASE_ANTEING:			
+			if self.players[self.curPlayer] != player:
+				return(WRONG_PLAYER_RESPONDED)
+
+			# Push the selected tile onto the pile
+			selected = self.decks[player.name].pop(position)
+			self.piles[player.name].append(selected)
+			
+			# Swap player
+			self.curPlayer += 1
+			self.curPlayer %= len(self.players)
+			return(0)
+		else if self.phase == PHASE_INITANTE:
+			if len(self.piles[player.name]) > 0:
+				return(PLAYER_ALREADY_ANTED)
+				
+			# Push the selected tile onto the pile
+			selected = self.decks[player.name].pop(position)
+			self.piles[player.name].append(selected)
+			
+			# Check if everyone has ante'd
+			self.curPlayer += 1
+			if self.curPlayer >= len(self.players):
+				self.phase = PHASE_ANTEING
+				return(INIT_PHASE_OVER)
 	
 	def playerBet(self, player, amount):
 		"""Bet a certain amount"""
-		if (self)
+		if self.phase != PHASE_BETTING or \
+			self.phase != PHASE_ANTEING:
+			return(WRONG_GAME_PHASE)
+		if self.players[self.curPlayer] != player:
+			return(WRONG_PLAYER_RESPONDED)
+		if int(amount) <= self.lastBet:
+			return(BET_TOO_LOW)
+		if int(amount) > self.totalTiles:
+			return(BET_TOO_HIGH)
+		
+		self.lastBet = int(amount)
+		self.lastBettingPlayer = player
+		self.phase = PHASE_BETTING
+		return(0)
 	
 	def getDefaultHand(self):
 		"""Return a starting hand for the player"""
@@ -123,23 +160,26 @@ class Skulls:
 			await self.bot.say(author + ": We are currently in the betting around: no more antes can be placed.")
 			return
 		
-		# Make sure the active player is playing
-		if author != session.getCurPlayer:
-			await self.bot.say(author + ": it is not your turn.\n" + \
-								"It is currently " + session.getCurPlayer + "'s turn.")
-			return
-	
-		if session.playerAnte(author, tile) == PLAYER_HAS_NO_SUCH_ANTE:# TODO: fix this line
+		retval = session.playerAnte(author, tile)
+		if retval == PLAYER_HAS_NO_SUCH_ANTE:# TODO: fix this line
 			await self.bot.say(author + ": that is not a valid tile\n" + \
 								"Your tiles have been DM'd to you")
 			await self.remind(ctx)
-			return
-	
-		# Player ante success -- go to next player
-		await self.bot.say(author + " has placed a tile. It is now " + session.getCurPlayer + "'s turn")
+		elif retval == WRONG_PLAYER_RESPONDED:
+			await self.bot.say(author + ": it is not your turn.\n")
+			await self.whichplayer(ctx)
+		elif retval == PLAYER_ALREADY_ANTED:
+			await self.bot.say("You have already laid your starting tile. Please wait for the rest of the players.")
+		else:
+			await self.bot.say(author + " has placed a tile.")
+			if retval == INIT_PHASE_OVER:
+				await self.bot.say("The game has begun!")
+			elif session.phase == PHASE_ANTEING:
+				await self.whichplayer(ctx)
 	
 	@commands.command(pass_context = True)
-	async def bet(self, ctx):
+	async def bet(self, ctx, amount):
+		"""Make a bet of how many you can flip in an active game"""
 		game_name = self.getGameName(ctx.message)
 		author = ctx.message.author.name
 		
@@ -150,9 +190,21 @@ class Skulls:
 		session = self.activeGames[game_name]
 		
 		# Begin the betting phase
-		session.phase = PHASE_INITANTE
+		retval = session.playerBet(amount)
 		
-		# Do more stuff
+		# Handle every possible retval
+		if retval == WRONG_GAME_PHASE:
+			await self.bot.say("You cannot bet now.")
+		elif retval == WRONG_PLAYER_RESPONDED:
+			await self.bot.say(author + ": it is not your turn.\n")
+			await self.whoseturn()
+		elif retval == BET_TOO_HIGH:
+			await self.bot.say("There aren't that many tiles to flip.")
+		elif retval == BET_TOO_LOW:
+			await self.bot.say("You must bet more than the last bet of " + str(session.lastBet))
+		elif retval == 0:
+			await self.bot.say(author + " has bet " + amount + "!")
+			await self.whichplayer(ctx)
 		
 	@commands.command(pass_context = True)
 	async def start(self, ctx):
@@ -172,8 +224,9 @@ class Skulls:
 		message += " ".join(session.players)
 		
 		await self.bot.say(message)
+		await self.whichplayer(ctx)
 	
-	@commands.command(pass_context = True)
+	@commands.command(aliases = ['remindme'], pass_context = True)
 	async def remind(self, ctx):
 		""" Remind a user what tiles they have """
 		game_name = self.getGameName(ctx.message)
@@ -184,11 +237,25 @@ class Skulls:
 		
 		session = self.activeGames[game_name]
 		
-		message = "Your tiles are: "
+		# Create the message that tells them what their tiles are
+		message = "Your tiles are:\n"
 		pile_idx = range(0, len(session.piles[author]))
 		zipped_hand = zip(pile_idx, session.piles[author])
 		message += "\n".join(str(i[0]) + ": " + i[1] for i in zipped_hand)
 		await self.bot.send_message(ctx.author, message)
+	
+	@commands.command(aliases = ['whichplayer'], pass_context = True)
+	async def whoseturn(self, ctx):
+		"""Remind the room whose turn it is"""
+		game_name = self.getGameName(ctx.message)
+		author = ctx.message.author.name
+		
+		if game_name not in self.activeGames.keys():
+			return
+		
+		session = self.activeGames[game_name]
+		
+		await self.bot.say("It is currently " + session.getCurPlayer + "'s turn.")
 	
 	@commands.command(pass_context = True)
 	async def down(self, ctx):
