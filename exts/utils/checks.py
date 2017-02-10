3e3a869f2cf7fc84de2d69c7e8d1b5c6ac39aa8f
OrdiNeu <OrdinaryNeumann@gmail.com>
2017-02-10 10:07:54 -0500

    Refactoring the administrator check

--- exts/utils/checks.py
+++ exts/utils/checks.py
@@ -5,9 +5,16 @@ import asyncio
 
 from discord.ext import commands
 
+# The keys below are the IDs of administrators permitted to run priveleged
+# commands. The values are not important
+admins = {
+	"135449740778274816": "OrdiNeu",
+	"136288437035597824": "Leg"
+}
+
 # Checks if a message came from the administrator
 def is_admin_check(message):
-	return "135449740778274816" == message.author.id or "136288437035597824" == message.author.id
+	return message.author.id in admins.keys()
 
 def is_admin():
     return commands.check(lambda ctx: is_admin_check(ctx.message))
