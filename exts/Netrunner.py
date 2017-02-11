c5d9d3ae7bc825583fd5ee06a2c314e77c04a5c0
Leg <dtstonecypher@gmail.com>
2017-02-11 13:43:01 -0500

    using non-discord emoji

--- exts/Netrunner.py
+++ exts/Netrunner.py
@@ -34,10 +34,11 @@ class Netrunner:
     @staticmethod
     def replace_api_text_with_emoji(api_string):
         api_string = re.sub("(\[click\])", ":clock7:", api_string)
-        return api_string
+        return emoji.emojize(api_string)
 
     @staticmethod
     def replace_emoji_with_api_text(emoji_string):
+        emoji_string = emoji.demojize(emoji_string)
         emoji_string = re.sub(":clock7:", "\[click\]", emoji_string)
         return emoji_string
 
