0569b693ea602eb46cbad497c557d26ff6765ac0
Leg <dtstonecypher@gmail.com>
2017-02-18 15:56:53 -0500

    printing AH cards with fewer requests to the upstream server, listing first 5 results, with number matching by name

--- exts/Arkham.py
+++ exts/Arkham.py
@@ -12,6 +12,14 @@ class Arkham:
 
     def __init__(self, bot):
         self.bot = bot
+        self.ah_api = [{}]
+        self.init_api = False
+
+    def refresh_ah_api(self):
+        self.ah_api = sorted([c for c in requests.get('https://arkhamdb.com/api/public/cards').json()],
+                             key=lambda card: card['name'])
+        self.init_api = True
+
 
     @commands.command(aliases=['arkham','arkhamhorror'])
     async def ah(self, *, cardname : str):
@@ -32,13 +40,18 @@ class Arkham:
             m_response = query_redirects[m_query]
         else:
             # Otherwise find and handle card names
-            m_cards = [c for c in requests.get('https://arkhamdb.com/api/public/cards').json() if c['name'].lower().__contains__(m_query)]
+            if not self.init_api:
+                self.refresh_ah_api()
+            m_cards = [c for c in self.ah_api if c['name'].lower().__contains__(m_query)]
             if len(m_cards) == 1:
                 m_response = "http://arkhamdb.com" + m_cards[0]['imagesrc']
             elif len(m_cards) == 0:
-                m_response = "Sorry, I cannot seem to find any card with these parameters."
+                m_response += "Sorry, I cannot seem to find any card with these parameters:\n"
+                m_response += "http://arkhamdb.com/find/?q=" + m_query.replace(" ", "+")
             else:
-                m_response = "http://arkhamdb.com/find?q=" + m_query.replace (" ","+")
+                for i, card in enumerate(m_cards)[:5]:
+                    m_response = "http://arkhamdb.com{0}\n".format(card['imagesrc'])
+                m_response += "[{0}/{1}]".format(i + 1, len(m_cards))
         await self.bot.say(m_response)
 
 
