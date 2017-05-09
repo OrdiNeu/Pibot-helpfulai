2f4e6dce13eeefa5be21b307b7350daf7afac731
OrdiNeu <OrdinaryNeumann@gmail.com>
2017-05-08 22:26:11 -0400

    Oh god it's starting to work

--- exts/utils/scrollable.py
+++ exts/utils/scrollable.py
@@ -21,7 +21,7 @@ class Scrollable(listener.RctListener):
         """Send the given message list, scrolled to the given position"""
         self.msg = await self.bot.send_message(channel, msg_list[cur_pos])
         await self.bot.add_reaction(self.msg, u"\u2B06") # Up arrow
-        await self.bot.add_reaction(self.msg, u"\u25BE") # Down arrow
+        await self.bot.add_reaction(self.msg, u"\u2B07") # Down arrow
         self.msg_list = msg_list
         self.cur_pos = cur_pos
         self.attach(channel)
@@ -30,7 +30,7 @@ class Scrollable(listener.RctListener):
         """Handle the scroll reaction"""
         if str(rct) == u"\u2B06":    # Up arrow
             self.cur_pos += 1
-        elif str(rct) == u"\u25BE":   # Down arrow
+        elif str(rct) == u"\u2B07":   # Down arrow
             self.cur_pos -= 1
         print("reaction: " + str(rct))   # Debugging code
         self.bot.edit_message(self.msg, self.msg_list[self.cur_pos])
