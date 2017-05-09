ec9376b8b59ca650a1eabff9d6bbb913dab8d8f7
OrdiNeu <OrdinaryNeumann@gmail.com>
2017-05-08 22:39:02 -0400

    Should be fixed now?

--- exts/utils/scrollable.py
+++ exts/utils/scrollable.py
@@ -16,7 +16,6 @@ class Scrollable(listener.RctListener):
         self.msg = None # TODO: aghh pylint stop it
         super().__init__(None)    # TODO: I screwed up somewhere with designing this
 
-    @asyncio.coroutine
     async def send(self, channel, msg_list, cur_pos=0):
         """Send the given message list, scrolled to the given position"""
         self.msg = await self.bot.send_message(channel, msg_list[cur_pos])
@@ -30,7 +29,11 @@ class Scrollable(listener.RctListener):
         """Handle the scroll reaction"""
         if str(rct) == u"\u2B06":    # Up arrow
             self.cur_pos += 1
+            if self.cur_pos >= len(msg_list):
+                self.cur_pos = 0
         elif str(rct) == u"\u2B07":   # Down arrow
             self.cur_pos -= 1
+            if self.cur_pos < 0:
+                self.cur_pos = len(msg_list)-1
         print("reaction: " + str(rct))   # Debugging code
-        self.bot.edit_message(self.msg, self.msg_list[self.cur_pos])
+        await self.bot.edit_message(self.msg, self.msg_list[self.cur_pos])
