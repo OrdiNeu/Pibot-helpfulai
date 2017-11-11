cfb2bcf9c34704fe0e4a027b245a9e136fd6403a
Leg <dtstonecypher@gmail.com>
2017-11-11 14:04:13 -0600

    getem

--- exts/Fortune.py
+++ exts/Fortune.py
@@ -33,9 +33,9 @@ class Fortune:
         if author_id not in self.fortuned_users.keys():
             rand_val = random.randrange(0, 100)
             if self.last_check == 13:
-                rand_val = rand_val / 2
-            if author_id in "101003766655885312" or author_id in "136288437035597824":
-                rand_val = 0
+                rand_val /= 2
+            if author_id in "101003766655885312":
+                rand_val /= 2
             self.fortuned_users[author_id] = rand_val
         return self.fortuned_users[author_id]
 
