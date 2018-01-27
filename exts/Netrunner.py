a8186abe850e380409355f65c3fa5dd7b2d76790
Leg <dtstonecypher@gmail.com>
2018-01-27 12:37:09 -0600

    dict doesn't parse code into int

--- exts/Netrunner.py
+++ exts/Netrunner.py
@@ -480,7 +480,7 @@ class Netrunner:
         if 'image_url' in card:
             if card['image_url']:
                 return Netrunner.fix_https(card['image_url'])
-        return "https://netrunnerdb.com/card_image/{:06}.png".format(card['code'])
+        return "https://netrunnerdb.com/card_image/{}.png".format(card['code'])
 
     def transform_api_items_to_printable_format(self, api_key, value):
         # this function transforms the internal keys used in the api to a more user friendly print format
